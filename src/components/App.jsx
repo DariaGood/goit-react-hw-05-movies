dc02adc686bfc3e83e5dd4d821a99e0a4b20446b
DariaGood <102043020+DariaGood@users.noreply.github.com>
2023-06-27 15:11:54 +0300

    update App Rev

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ const Home = lazy(() => import('../pages/Home'));
 const Movies = lazy(() => import('../pages/Movies'))
 const MovieDetails = lazy(() => import('../pages/MovieDetails'))
 const Cast = lazy(() => import('../components/Cast/Cast'));
-const Reviews = lazy(() => import('../components/Reviews/Reviews'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
 
 export const App = () => {
   return (
